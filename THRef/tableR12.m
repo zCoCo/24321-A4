32f0236677f82eea7c09a4c1cd6e1ef29f39c43a
zCoCo <connor.caxd@gmail.com>
2019-04-15 20:21:47 -0400

    R12 Data Import

--- THRef/tableR12.m
+++ THRef/tableR12.m
@@ -1,17 +1,12 @@
 function R12 = tableR12()
     % Load Raw Data:
     R12 = ETable.loadFromLineFile('R12 - Saturated Freon 12 Temp Table', 12, ...
-        ["Temperature", "Pressure", "Specific Volume, Sat. Liq.","Specific Volume, Sat. Vap.", "Specific Internal Energy, Sat. Liq.", "Specific Internal Energy, Sat. Vap.", "Specific Enthalpy, Sat. Liq.", "Specific Enthalpy, Evap.", "Specific Enthalpy, Sat. Vap.","Specific Entropy, Sat. Liq.","Specific Entropy, Sat. Vap.", "Temperature2 (ignore me)"],...
-        ["T","P", "vf","vg", "uf","ug", "hf","hfg","hg", "sf","sg", "T2"]);
+        ["Temperature", "Pressure", "Specific Volume, Sat. Liq.","Specific Volume, Sat. Vap.", "Density, Sat. Liq.", "Density, Sat. Vap.", "Specific Enthalpy, Sat. Liq.", "Specific Enthalpy, Evap.", "Specific Enthalpy, Sat. Vap.","Specific Entropy, Sat. Liq.","Specific Entropy, Sat. Vap.", "Temperature2 (ignore me)"],...
+        ["T","P", "vf","vg", "df","dg", "hf","hfg","hg", "sf","sg", "T2"]);
     
     % Convert Units to SI Base Units:
     R12.edit('T', R12.T + 273.15); % C -> K
-    R12.edit('P', R12.P * 1e5); % bar -> Pa
-    
-    R12.edit('vf', R12.vf * 1e-3); % to m^3/kg
-    
-    R12.edit('uf', R12.uf * 1e3); % kJ -> J
-    R12.edit('ug', R12.ug * 1e3); % kJ -> J
+    R12.edit('P', R12.P * 1e3); % kPa -> Pa
     
     R12.edit('hf', R12.hf * 1e3); % kJ -> J
     R12.edit('hfg', R12.hfg * 1e3); % kJ -> J
@@ -26,9 +21,6 @@ function R12 = tableR12()
     
     R12.rename('vf', 'Sat. Liquid Specific Volume [$^{m^3}/_{kg}$]');
     R12.rename('vg', 'Sat. Vapour Specific Volume [$^{m^3}/_{kg}$]');
-    
-    R12.rename('uf', 'Sat. Liquid Specific Internal Energy [$^{J}/_{kg}$]');
-    R12.rename('ug', 'Sat. Vapour Specific Internal Energy [$^{J}/_{kg}$]');
     
     R12.rename('hf', 'Sat. Liquid Specific Enthalpy [$^{J}/_{kg}$]');
     R12.rename('hfg', 'Evaporation Specific Enthalpy [$^{J}/_{kg}$]');
