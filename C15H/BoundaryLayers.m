d7cfcf7c0ba712de295d39b0c922b2de7ecfa840
zCoCo <connor.caxd@gmail.com>
2019-04-20 23:11:21 -0400

    updated table3

--- C15H/BoundaryLayers.m
+++ C15H/BoundaryLayers.m
@@ -15,43 +15,67 @@ function BoundaryLayers()
                 x = mean(tab.X(entry & tab.Y ~= 0));
                 Rex = mean(tab.Rex(entry & tab.Y ~= 0));
                 dRex = mean(tab.dRex(entry & tab.Y ~= 0));
-                Vinf = mean(tab.Vinf(entry & tab.Y ~= 0));
+                Vinf = max(Vs);%mean(tab.Vinf(entry & tab.Y ~= 0));
                 ym = mean(ys);
                 
                 syms symd
-                if Rex < 5e5 % Laminar:
-                    if(Rex~=0)
-                        d_Re = 5*x / Rex^(1/5) ;
+                % Laminar:
+                if(Rex~=0)
+                    d_Re_lam = 5*x / Rex^(1/5) ;
+                else
+                    d_Re_lam = 0;
+                end
+
+                A = sum(ys.^6); B = sum(ys.^4); C = sum(Vs.*ys.^3);
+                D = B; E = sum(ys.^2); F = sum(ys.*Vs);
+                denom = (A*E - B*D);
+                a = (C*E - F*B) / denom;
+                b = (A*F - C*D) / denom;
+
+                V = @(y) a*y.^3 + b*y;
+                SE = sqrt(sum((V(ys) - ym).^2) / (n-2));
+
+                d = double(solve(a*symd^3 + b*symd == 0.95.*Vinf, symd));
+                d_lam = d(d>0); d_lam = d_lam(1); % Select First Positive Solution
+                dd_lam = 1.96 * SE;
+                
+                % Turbulent:
+                if(Rex~=0)
+                    if(f == tab.OBSTACLE)
+                        d_Re_turb = x * (0.381/Rex^(1/5)) * (Rex~=0);
                     else
-                        d_Re = 0;
+                        d_Re_turb = x * (0.381/Rex^(1/5) - 10.256/Rex) * (Rex~=0);
                     end
+                else 
+                    d_Re_turb = 0;
+                end
+                c = sum(Vs.*ys.^(1/7)) ./ sum(ys.^(2/7));
+
+                V = @(y) c*y.^(1/7);
+                SE = sqrt(sum((V(ys) - ym).^2) / (n-2));
+
+                d = double(solve(c*symd^(1/7) == 0.95.* Vinf));
+                d_turb = d(d>0); d_turb = d_turb(1); % Select First Positive Solution
+                dd_turb = 1.96 * SE;
                     
-                    A = sum(ys.^6); B = sum(ys.^4); C = sum(Vs.*ys.^3);
-                    D = B; E = sum(ys.^2); F = sum(ys.*Vs);
-                    denom = (A*E - B*D);
-                    a = (C*E - F*B) / denom;
-                    b = (A*F - C*D) / denom;
-                    
-                    V = @(y) a*y.^3 + b*y;
-                    SE = sqrt(sum((V(ys) - ym).^2) / (n-2));
-                    
-                    d = double(solve(a*symd^3 + b*symd == 0.95.*Vinf, symd));
-                    d = d(d>0); d = d(1); % Select First Positive Solution
-                    dd = 1.96 * SE;
-                else % Turbulent:
-                    if(Rex~=0)
-                        d_Re = x * (0.381/Rex^(1/5) - 10.256/Rex) * (Rex~=0);
+                if f == tab.OBSTACLE || f == tab.ROUGH % Choose best fitting curve
+                    if dd_turb < dd_lam
+                        d_Re = d_Re_turb;
+                        d = d_turb;
+                        dd = dd_turb;
                     else
-                        d_Re = 0;
+                        d_Re = d_Re_lam;
+                        d = d_lam;
+                        dd = dd_lam;
                     end
-                    c = sum(Vs.*ys.^(1/7)) ./ sum(ys.^(2/7));
-                    
-                    V = @(y) c*y.^(1/7);
-                    SE = sqrt(sum((V(ys) - ym).^2) / (n-2));
-                    
-                    d = double(solve(c*symd^(1/7) == 0.95.* Vinf));
-                    d = d(d>0); d = d(1); % Select First Positive Solution
-                    dd = 1.96 * SE;
+                elseif Rex > 5e5
+                    d_Re = d_Re_turb;
+                    d = d_turb;
+                    dd = dd_turb;
+                else
+                    d_Re = d_Re_lam;
+                    d = d_lam;
+                    dd = dd_lam;
                 end
                 
                 % Add Entry to Table:
