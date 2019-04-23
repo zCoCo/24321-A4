68e7cde7a67b43560b597674a86e947786266a95
zCoCo <connor.caxd@gmail.com>
2019-04-23 13:14:27 -0400

    HTFin init

---
+++ HTFin/table1.m
@@ -0,0 +1,9 @@
+function table1()
+
+
+    function gradDescent()
+        
+        2*(C + (Tb*(cosh(A*h^(1/2)) + B*sinh(A*h^(1/2))))/(cosh(L*h^(1/2)) + B*sinh(L*h^(1/2))))*((A*Tb*(sinh(A*h^(1/2)) + B*cosh(A*h^(1/2))))/(h^(1/2)*(2*cosh(L*h^(1/2)) + 2*B*sinh(L*h^(1/2)))) - (L*Tb*(cosh(A*h^(1/2)) + B*sinh(A*h^(1/2)))*(sinh(L*h^(1/2)) + B*cosh(L*h^(1/2))))/(2*h^(1/2)*(cosh(L*h^(1/2)) + B*sinh(L*h^(1/2)))^2))
+    end
+
+end
